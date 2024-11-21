725bf92fc48671207566a65cbcd9011bbf942f36
pedrosrft <00001120314495sp@al.educacao.sp.gov.br>
2024-11-21 13:43:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fundação',
+    'Quando a Ponte Preta foi fundada?',
+    '1900'
+)
+
+criaCartao(
+    'Títulos',
+    'Quantos títulos oficiais a Ponte Preta possui?',
+    '0'
+)
+
+criaCartao(
+    'Atilharia',
+    'Qual é o maior atilheiro da Ponte Preta?',
+    'Dicá'
+)
+
+criaCartao(
+    'Mascote',
+    'Qual é o mascote da Ponte Preta?',
+    'Macaca'
+)
